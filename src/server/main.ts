d1f079c3bdc8feb6cc03ea39d5ff58dd7ce33690
Mykola <mykola.harkusha@idapgroup.com>
2019-03-22 12:16:26 +0200

    Update npm scripts and enable aot

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
+
+enableProdMode();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
